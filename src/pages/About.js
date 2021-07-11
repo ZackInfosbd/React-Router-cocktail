bf861f8cd095206ae131d2588c7cb98e242cece0
Zack DB DEV <zackinfosbd@gmail.com>
2021-07-11 16:59:49 +0100

    about + error pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta impedit
+        cupiditate, totam ab expedita incidunt ducimus eius! Accusamus amet
+        necessitatibus minus culpa odio rerum voluptatum est, obcaecati ducimus
+        quasi nisi. Quisquam perferendis, tempore fugit iste possimus
+        repellendus molestias molestiae! Fugiat!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
